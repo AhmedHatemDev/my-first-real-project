3ab230755a724b36ab39e04217a882b15c6f3a7e
Ahmed Hatem <ahmed.hatem.real53@gmail.com>
2026-04-04 15:13:42 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+import aiosqlite
+
+DATABASE_NAME = 'test.db'
+
+async def lifespan(app:FastAPI):
+    
+    async with aiosqlite.connect(DATABASE_NAME) as db:
+        await db.execute("""
+                CREATE TABLE IF NOT EXISTS tasks(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   task_name TEXT
+                   );
+
+""")
+        await db.commit()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/get_tasks")
+async def get_tasks():
+    async with aiosqlite.connect(DATABASE_NAME) as db:
+      
+        db.row_factory = aiosqlite.Row
+        cursor = await db.execute("SELECT * FROM tasks")
+        rows = await cursor.fetchall()
+        tasks = [dict(row) for row in rows]
+        
+    return {"Tasks":tasks}
+
+@app.post("/make_task")
+async def make_task(task:str):
+    async with aiosqlite.connect(DATABASE_NAME) as db:
+        await db.execute("INSERT INTO tasks (task_name) VALUES (?)",(task,))
+        await db.commit()
+        return {"message":f" task added succefully {task}"}
+
+@app.put("/update_task/{task_id}")
+async def update_task(task_id:int,update:str):
+    async with aiosqlite.connect(DATABASE_NAME) as db :
+        await db.execute("UPDATE tasks SET task_name = ? WHERE id = ? " ,(update,task_id))
+        await db.commit()
+        return {"message":f" task updated succefully {update}"}
+    return {"message":"something went wrong"}
+    
+
+@app.delete("/delte_task")
+async def delete_task(task_id:int):
+    async with aiosqlite.connect(DATABASE_NAME) as db:
+        await db.execute("DELETE FROM tasks WHERE id = ?",(task_id,))
+        await db.commit()
+        return {"message":"task deleted succefully"} 
+    
+
+# < ------------- NEXT TIME CREATE MORE ENDPOINTS TO PRACTICE SQL QUERIES --------------- >
+# < ------------- NEXT TIME CREATE MORE ENDPOINTS TO PRACTICE SQL QUERIES --------------- >
+# < ------------- NEXT TIME CREATE MORE ENDPOINTS TO PRACTICE SQL QUERIES --------------- >
